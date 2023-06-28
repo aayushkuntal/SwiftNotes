54c51da7e0cf854ed83afbeab4b55932fb1af403
Aayush Kuntal <85279026+aayushkuntal@users.noreply.github.com>
2023-06-28 18:26:16 +0530

    Update store.jsx to use @reduxjs/toolkit

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,40 +1,24 @@
-import { createStore, combineReducers, applyMiddleware} from "redux";
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
 import { userLoginReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
 import { noteCreateReducer, noteDeleteReducer, noteListReducer, noteUpdateReducer } from "./reducers/notesReducers";
 
-const reducers=combineReducers({
-
-    // reducers
-    //userLogin here is the state name and userLoginReducer is the reducer
-    //userLogin state helps to access the state in the component
-    //use of userLogin here is to access the state in the component
-    
-    userLogin:userLoginReducer,
-    userRegister:userRegisterReducer,
-    noteList:noteListReducer,
-    noteCreate:noteCreateReducer,
-    noteUpdate:noteUpdateReducer,
-    noteDelete:noteDeleteReducer,
-    userUpdate:userUpdateReducer
-});
-
-const userInfoFromStorage=localStorage.getItem('userInfo')?JSON.parse(localStorage.getItem('userInfo')):null;
-
-const intitalState={
-    userLogin:{userInfo:userInfoFromStorage}
+const reducers = {
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  noteList: noteListReducer,
+  noteCreate: noteCreateReducer,
+  noteUpdate: noteUpdateReducer,
+  noteDelete: noteDeleteReducer,
+  userUpdate: userUpdateReducer
 };
 
-const middleware =[thunk];
-
-const store = createStore(
-    reducers,
-    intitalState,
-    composeWithDevTools(
-      applyMiddleware(...middleware)
-      // other store enhancers if any
-    )
-  );
+const store = configureStore({
+  reducer: reducers,
+  preloadedState: {
+    userLogin: {
+      userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+    }
+  }
+});
 
 export default store;
